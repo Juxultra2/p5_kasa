5197d016c31098df64efee66f6ba2087cf725b25
Juxultra2 <jux2@live.fr>
2024-11-21 14:37:52 +0100

    projet start

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+    return <h1>Bienvenue sur Footer !</h1>;
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <p>petite p a la con</p>
+
+        </header>
+    );
+}
+
+export default Header;
